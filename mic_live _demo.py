c71a60fffe8d23de59c19f793cee7689e7df141a
HrGaertner <gaertnerjonathan@gamil.com>
2019-11-26 20:11:35 +0100

    Found mic live demo reanimated git

---
+++ mic_live _demo.py
@@ -0,0 +1,84 @@
+#Adapted from Güenter Bartsch https://goofy.zamia.org/zamia-speech/misc/kaldi_decode_live.py
+
+import logging
+
+from nltools               import misc
+from nltools.pulserecorder import PulseRecorder
+from nltools.vad           import VAD, BUFFER_DURATION
+from nltools.asr           import ASR, ASR_ENGINE_NNET3
+from optparse              import OptionParser
+
+PROC_TITLE                       = 'kaldi_live_demo'
+
+DEFAULT_VOLUME                   = 150
+DEFAULT_AGGRESSIVENESS           = 2
+
+# DEFAULT_MODEL_DIR                = '/opt/kaldi/model/kaldi-generic-de-tdnn_250'
+DEFAULT_MODEL_DIR                = '/opt/kaldi/model/kaldi-generic-en-tdnn_250'
+DEFAULT_ACOUSTIC_SCALE           = 1.0
+DEFAULT_BEAM                     = 7.0
+DEFAULT_FRAME_SUBSAMPLING_FACTOR = 3
+
+STREAM_ID                        = 'mic'
+
+#
+# init
+#
+
+misc.init_app(PROC_TITLE)
+
+print("Kaldi live demo V0.2")
+
+source         = options.source
+volume         = options.volume
+aggressiveness = options.aggressiveness
+model_dir      = options.model_dir
+
+#
+# pulseaudio recorder
+#
+
+rec = PulseRecorder (source_name=source, volume=volume)
+
+#
+# VAD
+#
+
+vad = VAD(aggressiveness=aggressiveness)
+
+#
+# ASR
+#
+
+print "Loading model from %s ..." % model_dir
+
+asr = ASR(engine = ASR_ENGINE_NNET3, model_dir = model_dir,
+          kaldi_beam = DEFAULT_BEAM, kaldi_acoustic_scale = DEFAULT_ACOUSTIC_SCALE,
+          kaldi_frame_subsampling_factor = DEFAULT_FRAME_SUBSAMPLING_FACTOR)
+
+
+#
+# main
+#
+
+rec.start_recording()
+
+print "Please speak."
+
+while True:
+
+    samples = rec.get_samples()
+
+    audio, finalize = vad.process_audio(samples)
+
+    if not audio:
+        continue
+
+    logging.debug ('decoding audio len=%d finalize=%s audio=%s' % (len(audio), repr(finalize), audio[0].__class__))
+
+    user_utt, confidence = asr.decode(audio, finalize, stream_id=STREAM_ID)
+
+    print "\r%s                     " % user_utt,
+
+    if finalize:
+        print
