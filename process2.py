268b28fdca8f99af62768174df90d55599e2e126
jonathan <gaertnerjonathan@gmail.com>
2020-01-02 17:18:49 +0100

    feature extraction hopefully finished

--- process2.py
+++ process2.py
@@ -6,35 +6,12 @@ def tag(liste):
         tagger = pickle.load(f)
         return tagger.tag(liste)
 
-with  open("tokenized.pickle", "rb") as f:
+with  open("final_raw.pickle", "rb") as f:
     tagged = pickle.load(f)
-
-print("Loaded")
-print()
-
-tags_set = ["PPER", "APPRART", "PWS", "NE", "PRELS", "KOKOM", "PIAT", "CARD", "VMINF", "PIS", "XY", "PTKANT", "PTKNEG", "APPR", "ADV", "KON", "VMFIN", "APZR", "ADJD", "PDS", "VVFIN", "PRF", "VAINF", "ADJA", "$.", "TRUNC", "VVPP", "PDAT", "ART", "NN", "PPOSAT", "VVINF", "$(", "VAPP", "$,", "PWAV", "KOUS", "KOUI", "FM", "VVIZU", "VVIMP", "VAFIN", "PTKZU", "PTKVZ", "PROAV"]
-
-tags = []
-
-for tag in tagged:
-    if tag in tags_set:
-        tags.append(tags_set.index(tag))
-    else:
-        print(tag)
-        tags_set.append(tag)
-        tags.append(tags_set.index(tag))
-print("processed")
-print()
-with open("final_raw.pickle", "wb") as f:
-    pickle.dump(tags, f)
-
-print(tags_set)
-print()
 
 tagged = tags
 feature = []
 vec = []
-punctation = []
 punctation2 = []
 punc_vec = []
 event = False
@@ -43,7 +20,7 @@ f_feature = []
 f_vector = []
 
 for i in range(0, len(tagged)):
-    if tagged[i] == "$.":
+    if tagged[i] == 24:
         event = True
         punctation = []
         punctation2 = []
@@ -62,15 +39,14 @@ for i in range(0, len(tagged)):
             f_feature.append(tagged)
             feature = punctation2
             vec = punc_vec
-            punctation, punctation2, punc_vec = [], [], []
+            punctation2, punc_vec = [], []
     print(i)
-    if len(punctation) >= 39:
-        punctation = []
+    if len(punctation2) >= 39:
         punctation2 = []
 while len(tagged) < 40:
     tagged.append(100)
     f_vector.append(vec)
-    f_feature.append(sentence)
+    f_feature.append(sen)
 
 with open('vec.pickle', 'wb') as f_vec:
     pickle.dump(f_vector, f_vec)
