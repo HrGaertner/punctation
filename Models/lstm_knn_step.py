53480500c54a240131721edb317ec51bde7974d0
Hr. Gaertner <gaertnerjonathan@gmail.com>
2020-02-12 12:18:43 +0100

    adding advices for better content

---
+++ Models/lstm_knn_step.py
@@ -0,0 +1,48 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import tensorflow.keras.callbacks
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding
+from tensorflow.keras.models import Model
+import pickle
+
+early = tensorflow.keras.callbacks.EarlyStopping(monitor='loss',
+                              min_delta=0.03,
+                              patience=2,
+                              verbose=0, mode='auto')
+
+
+(train_data, train_targets), (test_data, test_targets) = pickle.load(open("dataset.pickle", "rb"))
+
+train_data, train_targets = np.array(train_data), np.array(train_targets)
+"""
+train_data = pickle.load(open("feature.pickle", "rb")).tolist()
+train_data.append(train_data[0])
+train_targets = pickle.load(open("vec.pickle", "rb")).tolist()
+train_targets.append(train_targets[0])
+
+train_data = np.array(train_data)
+train_targets = np.array(train_targets)"""
+
+def create_lstm_model(vocab_size, embedding_size=None, embedding_weights=None):
+    message = Input(shape=40, dtype='int32', name='lstm_input')
+    embedding = Embedding(mask_zero=False, input_dim=55,
+                          output_dim=128,
+                          trainable=True,
+                          name='lstm_embedding')(message)
+
+    lstm_1 = LSTM(units=128, return_sequences=False)(embedding)
+    preds = Dense(40, activation='unit_step', name='lstm_predictions')(lstm_1)
+    
+    model = Model(
+        inputs=[message],
+        outputs=[preds],
+    )
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return model
+
+lstm_model = create_lstm_model(40,)
+lstm_model.save("lstm_punctator")
+lstm_model.summary()
+lstm_model.fit(train_data, train_targets, epochs=12, batch_size=1024, callbacks=[early])
+lstm_model.save_weights("lstm_punctator.h5")
+lstm_model.evaluate(test_data, test_targets)
